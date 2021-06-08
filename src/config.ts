82a33e3d1b9723bf83bdc3be69bd053778f842a0
Jack Yin <jackyin0@gmail.com>
2021-06-08 18:54:22 -0400

    better modularity

---
+++ src/config.ts
@@ -0,0 +1,30 @@
+import { config } from 'dotenv';
+config();
+
+const throwError = (err: string) => {
+  throw new Error(err);
+};
+
+export const SOLANA_RPC_URL = process.env.SOLANA_RPC_URL
+  ? process.env.SOLANA_RPC_URL
+  : throwError(`Must specify valid SOLANA_RPC_URL. Got: ${process.env.SOLANA_RPC_URL}`);
+
+export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY
+  ? process.env.AWS_ACCESS_KEY
+  : throwError(`Must specify valid AWS_ACCESS_KEY. Got: ${process.env.AWS_ACCESS_KEY}`);
+
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+  ? process.env.AWS_SECRET_ACCESS_KEY
+  : throwError(
+      `Must specify valid AWS_SECRET_ACCESS_KEY. Got: ${process.env.AWS_SECRET_ACCESS_KEY}`,
+    );
+
+export const REGION = process.env.REGION
+  ? process.env.REGION
+  : throwError(`Must specify valid REGION. Got: ${process.env.REGION}`);
+
+export const BUCKET = process.env.BUCKET
+  ? process.env.BUCKET
+  : throwError(`Must specify valid BUCKET. Got: ${process.env.BUCKET}`);
+
+export const FOLDER = process.env.FOLDER ? process.env.FOLDER : '';
---
+++ src/types.ts
@@ -0,0 +1,47 @@
+import { Event } from '@project-serum/serum/lib/queue';
+import { PublicKey } from '@solana/web3.js';
+import BN from 'bn.js';
+
+// Object that defines a market's metadata
+export type MarketMeta = {
+  address: string;
+  name: string;
+  deprecated: boolean;
+  programId: string;
+  baseCurrency?: string;
+  quoteCurrency?: string;
+  _baseSplTokenDecimals?: number;
+  _quoteSplTokenDecimals?: number;
+};
+
+export interface FullEvent extends Event {
+  clientOrderId: BN;
+  side: string;
+  price: number;
+  feeCost: number;
+  size: number;
+}
+
+export type FullEventMeta = {
+  address: string;
+  programId: string;
+  baseCurrency?: string;
+  quoteCurrency?: string;
+  isFill: boolean;
+  isOut: boolean;
+  isBid: boolean;
+  isMaker: boolean;
+  openOrdersSlot: number;
+  feeTier: number;
+  nativeQuantityRelease: BN;
+  nativeQuantityPaid: BN;
+  nativeFeeOrRebate: BN;
+  orderId: BN;
+  openOrders: PublicKey;
+  clientOrderId: BN;
+  side: string;
+  price: number;
+  feeCost: number;
+  size: number;
+  loadTimestamp: string;
+};
