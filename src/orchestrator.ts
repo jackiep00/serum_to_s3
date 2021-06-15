c35f05c2b0aaa3afb154be1d84f7bc6a1ec10cb4
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-06-15 15:36:47 -0400

    write basic orchestrator and use in index

--- src/orchestrator.ts
+++ src/orchestrator.ts
@@ -1,24 +1,29 @@
-import * as cron from 'node-cron';
-import { Market, MARKETS } from '@project-serum/serum';
-import { MarketMeta, FullEvent, FullEventMeta } from './types';
-import { pullAndSaveSerumEventsToCSV } from './pull_and_save_serum_events';
-import { writeFile, readFileSync } from 'fs';
-import { BATCH_FILENAME } from './config';
+import cron from 'node-cron';
+import { MARKETS } from '@project-serum/serum';
+import { MarketMeta } from './types';
+import { Connection } from '@solana/web3.js';
+import { SOLANA_RPC_URL } from './config';
+import { logger } from './utils';
+import { Scraper } from './scraper';
 
+const WAIT_TIME = 0;
+const NUM_PULLS_IN_BATCH = 75;
 const SECONDS_BETWEEN_RUNS = 3;
 const activeMarkets: MarketMeta[] = MARKETS.filter((item) => !item['deprecated']);
 
-// // TODO: generate a target filename and make it accessible
+export const orchestrator = async function () {
+  logger.info('Starting app');
+  const connection = new Connection(SOLANA_RPC_URL);
+  const activeMarkets: MarketMeta[] = MARKETS.filter((market) => !market['deprecated']);
 
-// cron.schedule(`*/${SECONDS_BETWEEN_RUNS} * * * * *`, () => {
-//   // cron.schedule(`*/3 * * * *`, () => {
-//   activeMarkets.forEach(async (market) => {
-//     pullAndSaveSerumEventsToCSV(market);
-//   });
-// });
+  // TODO: generate a target filename and make it accessible
 
-// // every hour
-// cron.schedule(`0 * * * * *`, () => {
-//   // attempt a batch upload?
-//   // metadataScraper.getParseSaveMetadataAsync(connection);
-// });
+  cron.schedule(`*/3 * * * *`, async () => {
+    logger.info('Starting run');
+    const scrapers: Scraper[] = activeMarkets.map((market) => new Scraper(market));
+    await Promise.all(scrapers.map((scraper) => scraper.getParseSaveEventsAsync(connection)));
+    logger.info('Batch run complete');
+  });
+};
+
+// await new Promise((resolve) => setTimeout(resolve, waitTime));
