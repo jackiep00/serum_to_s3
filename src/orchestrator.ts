953c078ed0191b6e63d507ce9d27f59093cf6b30
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-06-15 15:47:23 -0400

    update orchestrator

--- src/orchestrator.ts
+++ src/orchestrator.ts
@@ -6,24 +6,18 @@ import { SOLANA_RPC_URL } from './config';
 import { logger } from './utils';
 import { Scraper } from './scraper';
 
-const WAIT_TIME = 0;
-const NUM_PULLS_IN_BATCH = 75;
 const SECONDS_BETWEEN_RUNS = 3;
-const activeMarkets: MarketMeta[] = MARKETS.filter((item) => !item['deprecated']);
 
 export const orchestrator = async function () {
   logger.info('Starting app');
   const connection = new Connection(SOLANA_RPC_URL);
-  const activeMarkets: MarketMeta[] = MARKETS.filter((market) => !market['deprecated']);
+  const activeMarkets: MarketMeta[] = MARKETS.filter((market) => !market.deprecated);
+  const scrapers: Scraper[] = activeMarkets.map((market) => new Scraper(market));
 
   // TODO: generate a target filename and make it accessible
-
-  cron.schedule(`*/3 * * * *`, async () => {
+  cron.schedule(`*/${SECONDS_BETWEEN_RUNS} * * * * *`, async () => {
     logger.info('Starting run');
-    const scrapers: Scraper[] = activeMarkets.map((market) => new Scraper(market));
     await Promise.all(scrapers.map((scraper) => scraper.getParseSaveEventsAsync(connection)));
-    logger.info('Batch run complete');
+    logger.info('Run complete');
   });
 };
-
-// await new Promise((resolve) => setTimeout(resolve, waitTime));
